ec7935b9c7bafa2f474c8532d932702cb74e774e
corde171 <corde171@morris.umn.edu>
2018-03-01 14:00:14 -0600

    Add blank methods in UniversalDatabase for testing purposes.

--- server/src/main/java/umm3601/database/UniversalDatabase.java
+++ server/src/main/java/umm3601/database/UniversalDatabase.java
@@ -1,4 +1,24 @@
 package umm3601.database;
 
+import org.bson.Document;
+
+import java.util.List;
+
 public class UniversalDatabase {
+
+    public boolean addLoggedEmoji() {
+        return false;
+    }
+
+    public List<Document> getLoggedEmoji() {
+        return null;
+    }
+
+    public boolean addGoal() {
+        return false;
+    }
+
+    public List<Document> getGoal() {
+        return null;
+    }
 }
