93f706dbb922eb46ba1c004b2202ddb4f197476e
John Hoff <hoffx247@morris.umn.edu>
2018-03-01 20:00:01 -0600

    Create and Setup an ItemController #5
    The ItemController only handles goals as string objects for now. More will need to be added. Ids might also be handled well, but I'm unsure of how this works so I left in there.

---
+++ server/src/main/java/umm3601/database/ItemController.java
@@ -0,0 +1,94 @@
+package umm3601.database;
+
+import com.google.gson.Gson;
+import com.mongodb.MongoException;
+import com.mongodb.client.FindIterable;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import com.mongodb.util.JSON;
+import org.bson.Document;
+import org.bson.types.ObjectId;
+
+import java.util.Iterator;
+import java.util.Map;
+
+import static com.mongodb.client.model.Filters.eq;
+
+
+// Controller that manages information about people's items.
+public class ItemController {
+
+    private final Gson gson;
+    private MongoDatabase database;
+    private final MongoCollection<Document> itemCollection;
+
+    // Construct controller for items.
+    public ItemController(MongoDatabase database) {
+        gson = new Gson();
+        this.database = database;
+        itemCollection = database.getCollection("items");
+    }
+
+    public String getItem(String id) {
+        FindIterable<Document> jsonItems
+            = itemCollection
+            .find(eq("_id", new ObjectId(id)));
+
+        Iterator<Document> iterator = jsonItems.iterator();
+        if (iterator.hasNext()) {
+            Document item = iterator.next();
+            return item.toJson();
+        } else {
+            // We didn't find the desired item
+            return null;
+        }
+    }
+
+    // Helper method which iterates through the collection, receiving all
+    // documents if no query parameter is specified. If the goal parameter is
+    // specified, then the collection is filtered so only documents of that
+    // specified goal are found.
+    public String getItems(Map<String, String[]> queryParams) {
+
+        Document filterDoc = new Document();
+
+        // We will need more statements here for different objects,
+        // such as emoji, category, etc.
+
+        // "goal" will be a key to a string object, where the object is
+        // what we get when people enter their goals as a text body.
+        if (queryParams.containsKey("goal")) {
+            String targetContent = (queryParams.get("goal")[0]);
+            Document contentRegQuery = new Document();
+            contentRegQuery.append("$regex", targetContent);
+            contentRegQuery.append("$options", "i");
+            filterDoc = filterDoc.append("goal", contentRegQuery);
+        }
+
+        // FindIterable comes from mongo, Document comes from Gson
+        FindIterable<Document> matchingUsers = itemCollection.find(filterDoc);
+
+        return JSON.serialize(matchingUsers);
+    }
+
+    // As of now this only adds the goal, but you can separate multiple arguments
+    // by commas as we add them.
+    public String addNewItem(String goal) {
+
+        Document newItem = new Document();
+        newItem.append("goal", goal);
+        // Append new items here
+
+        try {
+            itemCollection.insertOne(newItem);
+            ObjectId id = newItem.getObjectId("_id");
+            System.err.println("Successfully added new item [_id=" + id + ", goal=" + goal);
+            // return JSON.serialize(newItem);
+            return JSON.serialize(id);
+        } catch(MongoException me) {
+            me.printStackTrace();
+            return null;
+        }
+    }
+
+}
