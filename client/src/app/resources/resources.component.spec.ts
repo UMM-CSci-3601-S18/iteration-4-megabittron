1f519327da5845486a84475c8039eb77094ec08c
welch298 <welch298@morris.umn.edu>
2018-03-07 21:22:15 -0600

    got a test to work
    #39

---
+++ client/src/app/resources/resources.component.spec.ts
@@ -0,0 +1,35 @@
+import {TestBed, ComponentFixture} from '@angular/core/testing';
+import {ResourcesComponent} from './resources.component';
+import {DebugElement} from '@angular/core';
+import {By} from '@angular/platform-browser';
+import {CustomModule} from '../custom.module';
+import {MATERIAL_COMPATIBILITY_MODE} from '@angular/material';
+
+describe('Resources', () => {
+
+    let component: ResourcesComponent;
+    let fixture: ComponentFixture<ResourcesComponent>;
+    let de: DebugElement;
+    let el: HTMLElement;
+
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            imports: [CustomModule],
+            declarations: [ResourcesComponent], // declare the test component
+            providers: [{provide: MATERIAL_COMPATIBILITY_MODE, useValue: true}],
+        });
+
+        fixture = TestBed.createComponent(ResourcesComponent);
+
+        component = fixture.componentInstance; // BannerComponent test instance
+
+        // query for the title <h1> by CSS element selector
+        de = fixture.debugElement.query(By.css('.main-top'));
+        el = de.nativeElement;
+    });
+
+    it('displays a greeting', () => {
+        fixture.detectChanges();
+        expect(el.textContent).toContain(component.title);
+    });
+});
