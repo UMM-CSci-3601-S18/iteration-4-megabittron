7f01b08203b82684e440e7e69ed035ece2a82951
CMenne <menne019@morris.umn.edu>
2018-03-21 21:45:49 -0500

    Add editGoal method Issue #18
    Also refactored every reference of "item" to "goal" for uniformity

--- server/src/main/java/umm3601/database/GoalRequestHandler.java
+++ server/src/main/java/umm3601/database/GoalRequestHandler.java
@@ -10,7 +10,7 @@ public class GoalRequestHandler {
     public GoalRequestHandler(GoalController goalController){
         this.goalController = goalController;
     }
-    /**Method called from Server when the 'api/items/:id' endpoint is received.
+    /**Method called from Server when the 'api/goals/:id' endpoint is received.
      * Get a JSON response with a list of all the users in the database.
      *
      * @param req the HTTP request
@@ -18,34 +18,34 @@ public class GoalRequestHandler {
      * @return one user in JSON formatted string and if it fails it will return text with a different HTTP status code
      */
 
-    // gets one item using its ObjectId--didn't use, just for potential future functionality
-    public String getItemJSON(Request req, Response res){
+    // gets one goal using its ObjectId--didn't use, just for potential future functionality
+    public String getGoalJSON(Request req, Response res){
         res.type("application/json");
         String id = req.params("id");
-        String item;
+        String goal;
         try {
-            item = goalController.getItem(id);
+            goal = goalController.getGoal(id);
         } catch (IllegalArgumentException e) {
             // This is thrown if the ID doesn't have the appropriate
             // form for a Mongo Object ID.
             // https://docs.mongodb.com/manual/reference/method/ObjectId/
             res.status(400);
-            res.body("The requested item id " + id + " wasn't a legal Mongo Object ID.\n" +
+            res.body("The requested goal id " + id + " wasn't a legal Mongo Object ID.\n" +
                 "See 'https://docs.mongodb.com/manual/reference/method/ObjectId/' for more info.");
             return "";
         }
-        if (item != null) {
-            return item;
+        if (goal != null) {
+            return goal;
         } else {
             res.status(404);
-            res.body("The requested item with id " + id + " was not found");
+            res.body("The requested goal with id " + id + " was not found");
             return "";
         }
     }
 
 
 
-    /**Method called from Server when the 'api/items' endpoint is received.
+    /**Method called from Server when the 'api/goals' endpoint is received.
      * This handles the request received and the response
      * that will be sent back.
      *@param req the HTTP request
@@ -54,10 +54,10 @@ public class GoalRequestHandler {
      */
 
     // Gets the goals from the DB given the query parameters
-    public String getItems(Request req, Response res)
+    public String getGoals(Request req, Response res)
     {
         res.type("application/json");
-        return goalController.getItems(req.queryMap().toMap());
+        return goalController.getGoals(req.queryMap().toMap());
     }
 
     /**Method called from Server when the 'api/users/new'endpoint is recieved.
@@ -68,14 +68,14 @@ public class GoalRequestHandler {
      * @param res the HTTP response
      * @return a boolean as whether the user was added successfully or not
      */
-    public String addNewItem(Request req, Response res)
+    public String addNewGoal(Request req, Response res)
     {
 
         res.type("application/json");
         Object o = JSON.parse(req.body());
         try {
             // if the object that is the JSON representation of the request body's class is the class BasicDBObject
-            // then try to add the item with goalController's addNewItem method
+            // then try to add the goal with goalController's addNewGoal method
             if(o.getClass().equals(BasicDBObject.class))
             {
                 try {
@@ -85,12 +85,12 @@ public class GoalRequestHandler {
                     String category = dbO.getString("category");
                     String goal = dbO.getString("name");
 
-                    System.err.println("Adding new item [goal=" + goal + ", category=" + category + " name=" + name + ']');
-                    return goalController.addNewItem(goal, category, name).toString();
+                    System.err.println("Adding new goal [goal=" + goal + ", category=" + category + " name=" + name + ']');
+                    return goalController.addNewGoal(goal, category, name).toString();
                 }
                 catch(NullPointerException e)
                 {
-                    System.err.println("A value was malformed or omitted, new item request failed.");
+                    System.err.println("A value was malformed or omitted, new goal request failed.");
                     return null;
                 }
 
@@ -102,6 +102,44 @@ public class GoalRequestHandler {
             }
         }
         catch(RuntimeException ree)
+        {
+            ree.printStackTrace();
+            return null;
+        }
+    }
+
+
+    public String editGoal(Request req, Response res)
+    {
+        res.type("application/json");
+        Object o = JSON.parse(req.body());
+        try {
+            if (o.getClass().equals(BasicDBObject.class))
+            {
+                try {
+                    BasicDBObject dbO = (BasicDBObject) o;
+
+                    String id = dbO.getString("_id");
+                    String name = dbO.getString("goal");
+                    String category = dbO.getString("category");
+                    String goal = dbO.getString("name");
+
+                    System.err.println("Editing goal [_id=" + id + "goal=" + goal + ", category=" + category + " name=" + name + ']');
+                    return goalController.editGoal(id, goal, category, name);
+                }
+                catch (NullPointerException e)
+                {
+                    System.err.println("A value was malformed or omitted, new journal request failed.");
+                    return null;
+                }
+            }
+            else
+            {
+                System.err.println("Expected BasicDBObject, received " + o.getClass());
+                return null;
+            }
+        }
+        catch (RuntimeException ree)
         {
             ree.printStackTrace();
             return null;
