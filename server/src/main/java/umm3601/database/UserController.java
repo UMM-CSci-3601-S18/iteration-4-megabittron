f3c472c31d369007a7d0dfef8e9d040b12a044ba
jubairmr9 <yasin011@morris.umn.edu>
2018-04-08 17:19:09 -0500

    added users.seed.json and started google user collection #6

---
+++ server/src/main/java/umm3601/database/UserController.java
@@ -0,0 +1,18 @@
+package umm3601.database;
+
+import com.google.gson.Gson;
+import com.mongodb.MongoException;
+import com.mongodb.client.FindIterable;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import com.mongodb.util.JSON;
+import org.bson.Document;
+import org.bson.types.ObjectId;
+
+import java.util.Date;
+import java.util.Iterator;
+import java.util.Map;
+
+
+public class UserController {
+}
---
+++ server/src/main/java/umm3601/database/UserRequestHandler.java
@@ -0,0 +1,11 @@
+package umm3601.database;
+
+import com.mongodb.BasicDBObject;
+import com.mongodb.util.JSON;
+import spark.Request;
+import spark.Response;
+
+public class UserRequestHandler {
+
+
+}
