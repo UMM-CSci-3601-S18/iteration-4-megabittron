76bce9c71d2849649d487188ef3c46bf0505a70a
john9hoff <hoffx247@morris.umn.edu>
2018-04-11 19:45:46 -0500

    Only show snackbar on success for goals #11
    Weird thing is that the goal deletes on err =>, so that's where I put the snackbar message. I don't know why, but I'm leaving it that way because it works. Snackbar messages have been refactored and added to: add goal, complete goal, and delete goal.

--- client/src/app/goals/goals.component.ts
+++ client/src/app/goals/goals.component.ts
@@ -70,13 +70,16 @@ export class GoalsComponent implements OnInit {
                 this.goalService.addNewGoal(result).subscribe(
                     addGoalResult => {
                         this.highlightedID = addGoalResult;
-                        this.refreshGoals();
                     },
                     err => {
                         // This should probably be turned into some sort of meaningful response.
                         console.log('There was an error adding the goal.');
                         console.log('The error was ' + JSON.stringify(err));
                     });
+                this.snackBar.open("Added Goal", "CLOSE", {
+                    duration: 2000,
+                });
+                this.refreshGoals();
             }
         });
     }
@@ -91,6 +94,9 @@ export class GoalsComponent implements OnInit {
                 console.log(err);
                 this.refreshGoals();
                 this.loadService();
+                this.snackBar.open("Deleted Goal", "CLOSE", {
+                    duration: 2000,
+                });
             }
         );
     }
@@ -110,6 +116,9 @@ export class GoalsComponent implements OnInit {
         this.goalService.completeGoal(updatedGoal).subscribe(
             completeGoalResult => {
                 this.highlightedID = completeGoalResult;
+                this.snackBar.open("Completed Goal", "CLOSE", {
+                    duration: 2000,
+                });
                 this.refreshGoals();
             },
             err => {
@@ -139,13 +148,6 @@ export class GoalsComponent implements OnInit {
                 console.log('There was an error completing the goal.');
                 console.log('The error was ' + JSON.stringify(err));
             });
-    }
-
-
-    openSnackBar(message: string, action: string) {
-        this.snackBar.open(message, action, {
-            duration: 2000,
-        });
     }
 
     public filterGoals(searchPurpose: string, searchCategory: string,
