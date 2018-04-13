e8e9e49f8c24350c305dbc9f5c376f1405fbaaf0
CMenne <menne019@morris.umn.edu>
2018-04-12 21:32:57 -0500

    fix function name in goals test

--- client/src/app/goals/goals.service.spec.ts
+++ client/src/app/goals/goals.service.spec.ts
@@ -1,4 +1,4 @@
-/*
+
 import {HttpClientTestingModule, HttpTestingController} from '@angular/common/http/testing';
 import {TestBed} from '@angular/core/testing';
 import {HttpClient} from '@angular/common/http';
@@ -191,7 +191,7 @@ describe('Goal list service: ', () => {
             frequency: "Daily"
         };
 
-        goalListService.completeGoal(completeGoal).subscribe(
+        goalListService.editGoal(completeGoal).subscribe(
             id => {
                 expect(id).toBe(family_id);
             }
@@ -204,4 +204,4 @@ describe('Goal list service: ', () => {
     });
 
 });
-*/
+
