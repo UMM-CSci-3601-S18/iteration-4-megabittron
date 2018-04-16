aef4b31b5c70a962f7b779ac6a162ddb4730b236
Xaivteev <yang3792@morris.umn.edu>
2018-04-15 19:28:42 -0500

    attempt to fix e2e tests

--- client/src/app/app.service.ts
+++ client/src/app/app.service.ts
@@ -14,7 +14,7 @@ export class AppService {
 
     public testingToggle(): void {
         //Change this to stop the testing set up
-        let toggle = true;
+        var toggle = true;
 
         if(!environment.production && toggle){
             localStorage.setItem("userID", "defaultUserID");
