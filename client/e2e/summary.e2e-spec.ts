4d96e64a9fa5afffeeda4fad0a45cc4e4ebf8409
blake376 <blake376@morris.umn.edu>
2018-04-23 22:39:54 -0500

    update E2E test to use class and pastX radio buttons
    Story #32

--- client/e2e/summary.e2e-spec.ts
+++ client/e2e/summary.e2e-spec.ts
@@ -30,44 +30,44 @@ describe('Summary', () => {
     it('Should click on Happy dropdown ', () => {
         SummaryPage.navigateTo();
         page.clickMoodDropdown();
-        element(by.id('md-option-1')).click();
+        element(by.className('md-option-1')).click();
     });
 
     // makes sure it clicks on the Sad labeled emotion dropdown
     it('Should click on Sad dropdown ', () => {
         SummaryPage.navigateTo();
         page.clickMoodDropdown();
-        element(by.id('md-option-2')).click();
+        element(by.className('md-option-2')).click();
     });
 
     // makes sure it clicks on the Meh labeled emotion dropdown
     it('Should click on Meh dropdown ', () => {
         SummaryPage.navigateTo();
         page.clickMoodDropdown();
-        element(by.id('md-option-3')).click();
+        element(by.className('md-option-3')).click();
     });
 
     // makes sure it clicks on the Mad labeled emotion dropdown
     it('Should click on Mad dropdown ', () => {
         SummaryPage.navigateTo();
         page.clickMoodDropdown();
-        element(by.id('md-option-4')).click();
+        element(by.className('md-option-4')).click();
     });
 
     // makes sure it clicks on the Anxious labeled emotion dropdown
     it('Should click on Anxious dropdown ', () => {
         SummaryPage.navigateTo();
         page.clickMoodDropdown();
-        element(by.id('md-option-5')).click();
+        element(by.className('md-option-5')).click();
     });
 
     // makes sure it clicks on the Happy labeled Mood dropdown and then all emotions dropdown
     it('Should click on Happy dropdown and then do process again with the all moods dropdown  ', () => {
         SummaryPage.navigateTo();
         page.clickMoodDropdown();
-        element(by.id('md-option-1')).click();
+        element(by.className('md-option-1')).click();
         page.clickMoodDropdown();
-        element(by.id('md-option-0')).click();
+        element(by.className('md-option-0')).click();
     });
 
 
@@ -89,44 +89,44 @@ describe('Summary', () => {
     it('Should click on dropdown 1', () => {
         SummaryPage.navigateTo();
         page.clickIntensityDropdown();
-        element(by.id('md-option-7')).click();
+        element(by.className('md-option-7')).click();
     });
 
     // makes sure it clicks on dropdown 2
     it('Should click on dropdown 2', () => {
         SummaryPage.navigateTo();
         page.clickIntensityDropdown();
-        element(by.id('md-option-8')).click();
+        element(by.className('md-option-8')).click();
     });
 
     // makes sure it clicks on dropdown 3
     it('Should click on dropdown 3', () => {
         SummaryPage.navigateTo();
         page.clickIntensityDropdown();
-        element(by.id('md-option-9')).click();
+        element(by.className('md-option-9')).click();
     });
 
     // makes sure it clicks on dropdown 4
     it('Should click on dropdown 4', () => {
         SummaryPage.navigateTo();
         page.clickIntensityDropdown();
-        element(by.id('md-option-10')).click();
+        element(by.className('md-option-10')).click();
     });
 
     // makes sure it clicks on dropdown 5
     it('Should click on dropdown 5', () => {
         SummaryPage.navigateTo();
         page.clickIntensityDropdown();
-        element(by.id('md-option-11')).click();
+        element(by.className('md-option-11')).click();
     });
 
     // makes sure it clicks on dropdown 1 then dropdown 'all'
     it('Should click on dropdown 1 and then dropdown \'all\'  ', () => {
         SummaryPage.navigateTo();
         page.clickIntensityDropdown();
-        element(by.id('md-option-7')).click();
+        element(by.className('md-option-7')).click();
         page.clickIntensityDropdown();
-        element(by.id('md-option-6')).click();
+        element(by.className('md-option-6')).click();
     });
 
     /** **/
@@ -141,7 +141,7 @@ describe('Summary', () => {
     it('Should click on the choose a start date text field ', () => {
         SummaryPage.navigateTo();
         page.clickStartDate();
-        element(by.id('startDate')).click();
+        element(by.className('startDate')).click();
     });
 
     // makes sure there is an end date text field
@@ -154,26 +154,26 @@ describe('Summary', () => {
     it('Should click on the choose an end date text field ', () => {
         SummaryPage.navigateTo();
         page.clickEndDate();
-        element(by.id('endDate')).click();
+        element(by.className('endDate')).click();
     });
 
     // makes sure it clicks on the choose start date calendar button
     it('Should click on the choose a start date calendar button ', () => {
         SummaryPage.navigateTo();
-        element(by.id('startButton')).click();
+        element(by.className('startButton')).click();
     });
 
     // makes sure it clicks on the choose end date calendar button
     it('Should click on the choose an end date calendar button ', () => {
         SummaryPage.navigateTo();
-        element(by.id('endButton')).click();
+        element(by.className('endButton')).click();
     });
 
 
     // Should click on the clear date filters button
     it('Should click on the clear date filters button', () => {
         SummaryPage.navigateTo();
-        element(by.id('clearDatesButton')).click();
+        element(by.className('clearDatesButton')).click();
     });
 
 
@@ -195,66 +195,57 @@ describe('Summary', () => {
     it('Should click on dropdown day', () => {
         SummaryPage.navigateTo();
         page.clickTimeDropdown();
-        element(by.id('md-option-12')).click();
+        element(by.className('md-option-12')).click();
     });
 
     // makes sure it clicks on dropdown 1 then dropdown 'all'
     it('Should click on dropdown 1 and then dropdown \'all\'  ', () => {
         SummaryPage.navigateTo();
         page.clickTimeDropdown();
-        element(by.id('md-option-12')).click();
+        element(by.className('md-option-12')).click();
         page.clickTimeDropdown();
-        element(by.id('md-option-13')).click();
+        element(by.className('md-option-13')).click();
     });
 
     // makes sure it clicks on dropdown month
     it('Should click on dropdown month', () => {
         SummaryPage.navigateTo();
         page.clickTimeDropdown();
-        element(by.id('md-option-14')).click();
+        element(by.className('md-option-14')).click();
     });
 
     // makes sure it clicks on dropdown year
     it('Should click on dropdown year', () => {
         SummaryPage.navigateTo();
         page.clickTimeDropdown();
-        element(by.id('md-option-15')).click();
+        element(by.className('md-option-15')).click();
     });
 
     /** **/
 
-    it('Should click on the past X box', () => {
+    it('Should click on the past X radios', () => {
         SummaryPage.navigateTo();
-        element(by.id('pastXBox')).click();
+        element(by.className('pastXYes')).click();
+        element(by.className('pastXNo')).click();
     });
 
     it('Should click on the colorblind box', () => {
         SummaryPage.navigateTo();
-        element(by.id('colorblindBox')).click();
+        element(by.className('colorblindBox')).click();
     });
 
 
     it('Should click on the bar graph then line graph radio', () => {
         SummaryPage.navigateTo();
-        element(by.id('barradio')).click();
-        element(by.id('lineradio')).click();
+        element(by.className('barradio')).click();
+        element(by.className('lineradio')).click();
     });
 
-
-    it('Should click on the summary button', () => {
-        SummaryPage.navigateTo();
-        element(by.id('filterBasicButton')).click();
-    });
-
-    it('Should click on the detailed button', () => {
-        SummaryPage.navigateTo();
-        element(by.id('filterDetailedButton')).click();
-    });
 
     it('Should click on the list tab then graph tab', () => {
         SummaryPage.navigateTo();
-        element(by.id('md-tab-label-0-1')).click();
-        element(by.id('md-tab-label-0-0')).click();
+        element(by.className('mat-tab-label mat-ripple ng-star-inserted')).click();
+        element(by.className('mat-tab-label mat-ripple ng-star-inserted')).click();
     });
 
 });
