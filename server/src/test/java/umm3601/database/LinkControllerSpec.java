7f1fb7b9d3cfeef2103a307f6dfe3abb92c37dde
ahnafy <yasin011@morris.umn.edu>
2018-05-01 14:34:50 -0500

    added test file for linkcontroller #1 #4 #7

---
+++ server/src/test/java/umm3601/database/LinkControllerSpec.java
@@ -0,0 +1,18 @@
+package umm3601.database;
+
+import com.mongodb.BasicDBObject;
+import com.mongodb.MongoClient;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import org.bson.*;
+import org.bson.codecs.*;
+import org.bson.codecs.configuration.CodecRegistries;
+import org.bson.codecs.configuration.CodecRegistry;
+import org.bson.json.JsonReader;
+import org.bson.types.ObjectId;
+import org.junit.Before;
+import org.junit.Test;
+import umm3601.database.resource.LinksController;
+
+public class LinkControllerSpec {
+}
